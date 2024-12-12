6afb57438558efc9fcd08e95ca3e3c2150c8497a
Jose Sancho <jose.sancho89@gmail.com>
2024-12-12 23:40:46 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function getExcuse() {
+  let excuse = "";
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = [" ate", " peed", " crushed", " broke"];
+  let what = [" my homework", " my phone", " the car"];
+  let when = [
+    " before the class",
+    " when I was sleeping",
+    " while I was exercising",
+    " during my lunch",
+    " while I was praying"
+  ];
+
+  excuse += who[Math.floor(Math.random() * who.length)];
+  excuse += action[Math.floor(Math.random() * action.length)];
+  excuse += what[Math.floor(Math.random() * what.length)];
+  excuse += when[Math.floor(Math.random() * when.length)];
+
+  document.getElementById("myExcuse").innerHTML = excuse;
+
+  console.log(excuse);
+}
+
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+  getExcuse();
 };
